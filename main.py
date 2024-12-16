471c95d575d1ea80c4b3f0b7b500198401dd97b2
santosh gouda <santosh.gouda@transformco.com>
2024-12-16 19:04:30 +0530

    IC

---
+++ main.py
@@ -0,0 +1,14 @@
+from collections import (
+    Counter,
+    ChainMap,
+    defaultdict,
+    deque,
+    OrderedDict,
+    UserDict,
+    UserList,
+    UserString,
+)
+
+print("Hello Python Programming!!!!")
+
+x = 12
---
+++ main.sh
@@ -0,0 +1,7 @@
+#!"C:/Program Files/Git/bin/bash"
+
+echo "Hello from bash programming!!!!"
+name="Santosh peter "
+
+echo "Welcome to bash progrmming, $name"
+
